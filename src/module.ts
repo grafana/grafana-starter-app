a7c4b363d10b583eedd0910485bbc0c8cf0da943
Marcus Olsson <marcus.olsson@hey.com>
2021-03-01 14:15:49 +0100

    Remove legacy components

--- src/module.ts
+++ src/module.ts
@@ -1,19 +1,10 @@
 import { ComponentClass } from 'react';
 
-// Angular pages
-import { ExampleConfigCtrl } from './legacy/config';
-import { AngularExamplePageCtrl } from './legacy/angular_example_page';
 import { AppPlugin, AppRootProps } from '@grafana/data';
 import { ExamplePage1 } from './config/ExamplePage1';
 import { ExamplePage2 } from './config/ExamplePage2';
 import { ExampleRootPage } from './ExampleRootPage';
 import { ExampleAppSettings } from './types';
-
-// Legacy exports just for testing
-export {
-  ExampleConfigCtrl as ConfigCtrl,
-  AngularExamplePageCtrl, // Must match `pages.component` in plugin.json
-};
 
 export const plugin = new AppPlugin<ExampleAppSettings>()
   .setRootPage((ExampleRootPage as unknown) as ComponentClass<AppRootProps>)
